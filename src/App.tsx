09a029da302f9b0d5c0aa1a0040b161c4d8868a8
kenza17-urc <khammoudi17@gmail.com>
2023-07-05 15:00:25 +0100

    modif

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
-import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./component/Layout/Navbar";
+import Home from "./page/Home";
+import Formulaire from "./component/Formulaire/Formulaire";
+
 import { RecetteProvider } from "./lib/contexts/recetteContext";
 
-function App() {
+const App  = () => {
   return (
     <RecetteProvider>
-      <div></div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recette" element={<Formulaire />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </Router>
     </RecetteProvider>
   );
-}
+};
 
 export default App;
+
--- src/component/Layout/Navbar.tsx
+++ src/component/Layout/Navbar.tsx
@@ -1,9 +1,29 @@
 
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/recette" className="navbar-link">
+            Recette
+          </Link>
+        </li>
+        <li className="navbar-item">
+          <Link to="/contact" className="navbar-link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
