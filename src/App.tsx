d0c4c11ab4f5b599c3ded12f5db3f1da5177c5f1
PC-MASTERRACE\damie <damiens94160@gmail.com>
2023-07-05 16:37:40 +0200

    form recette

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Layout/Navbar";
 import Home from "./page/Home";
-import Formulaire from "./component/Formulaire/Formulaire";
+import Form from './page/Form'
 
 import { RecetteProvider } from "./lib/contexts/recetteContext";
 import DetailRecette from "./page/DetailRecette";
@@ -13,7 +13,7 @@ const App = () => {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/recette" element={<Formulaire />} />
+          <Route path="/recette" element={<Form />} />
           <Route path="/recette/:id" element={<DetailRecette />} />
           {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useState } from 'react';
-import FormRecette from '../component/Recette/FormRecette';
-
-interface Recipe {
-    name: string;
-    photo: string;
-    time: number;
-}
-
-const Home = () => {
-
-    const [recettes, setRecettes] = useState<Recipe[]>([])
-
-    const deleteRecipe = (index: number) => {
-        const newRecettes = [...recettes];
-        newRecettes.splice(index, 1);
-        setRecettes(newRecettes);
-      };
-    
-      const addRecipe = (recipe: Recipe) => {
-        const newRecettes = [...recettes, recipe];
-        setRecettes(newRecettes);
-      };
-
-    return (
-        <div>
-            <FormRecette onSubmit={addRecipe} />
-        </div>
-    );
-};
-
-export default Home;
-=======
-
-
 const Home = () => {
   return (
     <div>Home</div>
@@ -42,5 +5,3 @@ const Home = () => {
 }
 
 export default Home
-
->>>>>>> 4ad48de19979f1d0cc3c91ff481e21da0c73bc77
