67c9649dd4c80dd44bc94ff98c38b83f229fbf8d
Khalil El Ghoul <khalilelghoul01@gmail.com>
2023-07-06 11:14:51 +0200

    update

--- src/lib/contexts/recetteContext.tsx
+++ src/lib/contexts/recetteContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
 import { Recette } from "../models/recette";
 
 const RecetteContext = createContext<{
@@ -15,9 +15,10 @@ const RecetteContext = createContext<{
   },
   getRecette: () => undefined,
   recettes: [],
+
 });
 
-const randomId = () => {
+export const randomId = () => {
   return (
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15)
@@ -29,33 +30,11 @@ export const RecetteProvider = ({
 }: {
   children: React.ReactNode;
 }) => {
-  const [recettes, setRecettes] = useState<Recette[]>([
-    {
-      id: "10000",
-      titre: "titre",
-      description: "description",
-      image:
-        "https://images.unsplash.com/photo-1518779578993-ec3579fee39f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=435&q=80",
-      duree: 10,
-      tags: ["tag", "tag"],
-      ingredients: [
-        {
-          nom: "nom",
-          quantite: 10,
-          unite: "unite",
-        },
-      ],
-    },
-  ]);
+  const [recettes, setRecettes] = useState<Recette[]>([]);
 
   const addRecette = (recette: Omit<Recette, "id">) => {
-    setRecettes([
-      ...recettes,
-      {
-        ...recette,
-        id: randomId(),
-      },
-    ]);
+    const newRecettes = [...recettes, { ...recette, id: randomId() }];
+    setRecettes(newRecettes);
   };
 
   const removeRecette = (id: string) => {
@@ -65,6 +44,8 @@ export const RecetteProvider = ({
   const getRecette = (id: string) => {
     return recettes.find((recette) => recette.id === id);
   };
+
+  
   return (
     <RecetteContext.Provider
       value={{
--- src/page/Form.tsx
+++ src/page/Form.tsx
@@ -24,7 +24,7 @@ const Home = () => {
 
   return (
     <div>
-      <FormRecette onSubmit={addRecipe} />
+      <FormRecette  />
     </div>
   );
 };
