26b9534fe227f941c5f7dac007c541ce602371ad
PC-MASTERRACE\damie <damiens94160@gmail.com>
2023-07-06 14:53:10 +0200

    nombre de personnes

--- src/page/DetailRecette.tsx
+++ src/page/DetailRecette.tsx
@@ -1,8 +1,13 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { useRecette } from "../lib/contexts/recetteContext";
 import { Recette } from "../lib/models/recette";
 import '../component/Recette/DetailRecette.css'
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface FormInputs {
+  persons: number;
+}
 
 function minuteFormat(minutes: number) {
   const hours = Math.floor(minutes / 60);
@@ -19,14 +24,30 @@ function minuteFormat(minutes: number) {
 
   return result.trim(); 
 }
+
 function DetailRecette() {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
   const { getRecette } = useRecette();
-  const [recette, setRecette] = useState<Recette>();
+  const [recette, setRecette] = useState<Recette | undefined>();
+  const [counter, setCounter] = useState(0);
+  const { register, handleSubmit, formState: { errors } } = useForm<FormInputs>();
+  const counterRef = useRef<number>(0);
+  const [persons, setPersons] = useState(1)
+
   useEffect(() => {
     setRecette(getRecette(id || ""));
-  }, []);
+  }, [id, getRecette]);
+
+  useEffect(() => {
+    counterRef.current = counter;
+  }, [counter]);
+
   if (!recette) return null;
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    setPersons(data.persons);
+  };
+
   return (
     <div className="detail-recette-container">
       <img
@@ -46,15 +67,21 @@ function DetailRecette() {
           <span key={i} className="detail-recette-ingredient-item">
             <span className="detail-recette-ingredient-name">{ing.nom}</span>
             <span className="detail-recette-ingredient-quantity">
-              {ing.quantite}
+              {ing.quantite * persons}
             </span>
             <span className="detail-recette-ingredient-unit">{ing.unite}</span>
           </span>
         ))}
       </p>
+      <form style={{display: "flex", flexDirection: "row"}} onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input style={{width: "20px", marginRight: "10px"}} {...register('persons', { required: true })} />
+          {errors.persons && <span>Ce champ est requis</span>}
+        </div>
+        <button type="submit">Nombre de personnes</button>
+      </form>
     </div>
   );
-  
 }
 
 export default DetailRecette;
